863475c6b523783c6043c08c2696e587523f9d5f
Davidvboxt <davidvb00@gmail.com>
2023-02-13 09:38:07 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
+const url = 'https://whois.fdnd.nl/api/v1/member/david-van-boxtel'
+const data = await fetch(url).then((response) => response.json())
+
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -14,14 +18,15 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index', data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 4000)
 
 // Start express op, haal het ingestelde poortnummer op
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
